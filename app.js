ae7e84f64f8955f719777256bda7a8e2657037c6
FABI0FERREIRA <154641904+FABI0FERREIRA@users.noreply.github.com>
2024-09-06 18:17:37 -0300

    Update app.js
    now

--- app.js
+++ app.js
@@ -4,23 +4,56 @@ function pesquisar() {
     let section = document.getElementById("resultados-pesquisa")
     /*console.log(section)*/
     
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
     
-    let resualtados = ""
+
+
+
+
+    // se campo vazio for uma string sem nada 
+    if (!campoPesquisa ){
+        section.innerHTML = "<p>Nada foi encontrado.</p> <p>Você precisa digitar o nome de um gato ou cachorro o que vem mente sobre eles<p>"
+        return
+    }
+
     
-    
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+
+    let resualtados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    console.log(campoPesquisa)
     // para cada dados dentro da lista de dados 
     for (let dado of dados) {
-    
-        resualtados += `
-        <div class="item-resultado">
-            <h2><a href="#" target="_blank">${dado.titulo}</a></h2>
-        <p class="descricao-meta">${dado.descricao}</p>
-        <a href=${dado.link} target="_blank">Mais informações</a>
-        </div>`
-    
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        // se titulo faca 
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+            //criar um novo elemento
+            resualtados += `
+            <div class="item-resultado">
+                <h2><a href="#" target="_blank">${dado.titulo}</a></h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>`
+        
+        }
+
+        //console.log(dado.titulo.includes(campoPesquisa))
+
+
+
     
     }
     
+    if (!resualtados) {
+        resualtados = "<p>não foi encontrado.<p>"
+    }
+
     
     section.innerHTML = resualtados
 
