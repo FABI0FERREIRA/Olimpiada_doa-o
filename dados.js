e9f87a5aa3594aae0d733064fc92b921db51852a
FABI0FERREIRA <154641904+FABI0FERREIRA@users.noreply.github.com>
2024-09-06 18:18:06 -0300

    Update dados.js
    now

--- dados.js
+++ dados.js
@@ -3,35 +3,48 @@ let dados = [
 {
         titulo: "Caramelo",
         descricao: "O corredor:  O Caramelo é um verdadeiro raio! Com sua pelagem dourada brilhante e corpo esguio, ele é a definição de velocidade e agilidade.  Inspirando-se nos atletas de rua, esse cãozinho é capaz de alcançar velocidades impressionantes, chegando a correr até 80 quilômetros por hora atrás de uma moto. Sua energia é contagiante e sua paixão por correr é inegável, tornando-o um companheiro perfeito para aventuras ao ar livre.",
-        link: "https://www.adotepetz.com.br/"
+        link: "https://www.adotepetz.com.br/",
+        tags: "dourado, veloz, ágil, energético, aventureiro, cachorro, pet, adotado, vira-lata, icone"
 },
 {
         titulo: "Pinscher",
         descricao: "O luatdor: O Pinscher, com sua estatura imponente de 70 centímetros, é a personificação da valentia. Seus olhos penetrantes e expressão determinada revelam um espírito guerreiro, pronto para enfrentar qualquer desafio. Apesar de seu tamanho compacto, ele possui uma coragem desproporcional, defendendo seu território com ferocidade. Um verdadeiro gigante em um corpo pequeno.",
-        link: "https://www.adotepetz.com.br/"
+        link: "https://www.adotepetz.com.br/",
+        tags: "lutador, guerreiro, valente, pequeno porte, coragem, determinação, força, agility"
 },
 {
        
         titulo: "Rottweiler",
         descricao: "O salto em distançia: O Rottweiler, quando filhote, é a própria fofura, com sua pelagem preta brilhante e olhos castanhos expressivos. Cheio de energia, ele adora brincar e explorar o mundo ao seu redor. Conforme cresce, revela seu lado protetor, tornando-se um guardião leal e confiável de sua família. Apesar de sua aparência imponente, ele é um gigante gentil com aqueles que ama.",
-        link: "https://www.adotepetz.com.br/"
+        link: "https://www.adotepetz.com.br/",
+        tags: "rottweiler, preto, marrom, forte, protetor, leal, gentil, brincalhão, energético, afetuoso, guardião, família, cão de guarda, companheiro, adestravel, ativo, inteligente, grande porte, salto, filhote"
+
 }
 ,
 {
         titulo: "Siamês",
         descricao: "A ginasta artitistica: Com olhos azuis que parecem penetrar na alma e um corpo esbelto e elegante, o Siamês é um verdadeiro enigma. Sua voz melodiosa e exigente o torna um companheiro fiel e falante, sempre pronto para uma conversa. Sua pelagem bicolor, com pontos mais escuros, contrasta com sua personalidade expressiva, tornando-o um felino inesquecível.",
-        link: "https://www.adotepetz.com.br/"
+        link: "https://www.adotepetz.com.br/",
+        tags: "siames, gato, azul, elegante, falante, exigente, bicolor, expressivo, ginasta"
 },
 {
         titulo: "Maine Coon",
         descricao: "Jogador de basquete: O Maine Coon é um gigante gentil, com uma pelagem espessa que o protege do frio e uma personalidade afetuosa. Seus tufos de pelos nas orelhas e sua cauda longa e plumosa o tornam um verdadeiro felino nórdico. Sua natureza independente e curiosa o torna um companheiro ideal para famílias ativas.",
-        link: "https://www.adotepetz.com.br/"
+        link: "https://www.adotepetz.com.br/",
+        tags: "mainecoon, gato, gigante, gentil, peludo, nórdico, independente, curioso, basquete"
  },
  {
-titulo: "Persa",
-descricao: "Snowboard: Com uma pelagem exuberante e um rosto adocicado, o Persa é a personificação da realeza felina. Seus olhos grandes e expressivos transmitem uma aura de mistério e elegância. Apesar de sua aparência delicada, ele é um companheiro tranquilo e afetuoso, adorando receber carinho e atenção.",
-link: "https://www.adotepetz.com.br/"
+        titulo: "Persa",
+        descricao: "Snowboard: Com uma pelagem exuberante e um rosto adocicado, o Persa é a personificação da realeza felina. Seus olhos grandes e expressivos transmitem uma aura de mistério e elegância. Apesar de sua aparência delicada, ele é um companheiro tranquilo e afetuoso, adorando receber carinho e atenção.",
+        link: "https://www.adotepetz.com.br/",
+        tags: "persa, gato persa, peludo, longo pelo, olhos grandes, elegante, rosto adocicado, pelagem exuberante, calmo, tranquilo, afetuoso, carinhoso, dócil, real, nobre, snowboard, relaxado"
+
 }
+
+
+
+
+
 
 
 ]
